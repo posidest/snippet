ffa3bbdab084f5953ccd5bdf8a8e82aafdaa23fa
Alana <alana.lapoint@gmail.com>
2021-02-18 16:06:57 -0500

    login api route

---
+++ backend/db/seeders/20210218182017-demo-user.js
@@ -0,0 +1,39 @@
+'use strict';
+const faker = require('faker');
+const bcrypt = require('bcryptjs');
+
+
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [
+      {
+        email: 'demo@user.io',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password'),
+      },
+      {
+        email: faker.internet.email(),
+        username: faker.internet.userName(),
+        hashedPassword: bcrypt.hashSync(faker.internet.password())
+      },
+      {
+        email: faker.internet.email(),
+        username: faker.internet.userName(),
+        hashedPassword: bcrypt.hashSync(faker.internet.password())
+      },
+      {
+        email: faker.internet.email(),
+        username: faker.internet.userName(),
+        hashedPassword: bcrypt.hashSync(faker.internet.password())
+      },
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Users', {
+      id: { [Op.gt]: 0 }
+    }, {});
+  }
+};
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,48 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { restoreUser } = require('../../utils/auth.js');
+const { requireAuth } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+
+//get /api/set-token-cookie
+router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'Demo-lition'
+        },
+    })
+    setTokenCookie(res, user);
+    return res.json({ user });
+}));
+
+
+// GET /api/restore-user
+
+router.get(
+    '/restore-user',
+    restoreUser,
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
+
+// GET /api/require-auth
+
+router.get(
+    '/require-auth',
+    requireAuth,
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
+
 
 router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
